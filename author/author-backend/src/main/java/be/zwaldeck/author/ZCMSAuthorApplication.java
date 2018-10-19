252cbb148a8ca259d64569fb0504706a96eaf6d5
Wout Schoovaerts <schoovaertswout@live.be>
2018-10-19 19:47:42 +0200

    Maven setup

---
+++ author/author-backend/src/main/java/be/zwaldeck/author/ZCMSAuthorApplication.java
@@ -0,0 +1,12 @@
+package be.zwaldeck.author;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZCMSAuthorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZCMSAuthorApplication.class, args);
+    }
+}
